e37e47346bbbcb22e7cbd3ad3c71eca625edfe60
MicaildeJunior <micaoliveira.tecnologo@gmail.com>
2023-09-18 18:38:14 -0300

    Adicionar arquivos de projeto.

---
+++ Blog/Data/BlogDataContext.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data
+{
+    public class BlogDataContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}

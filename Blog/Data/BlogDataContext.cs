c7457f0653ebfbe5b6026b0578c666a1259a6066
MicaildeJunior <micaoliveira.tecnologo@gmail.com>
2023-09-18 20:57:49 -0300

    Atualizando Colunas do Banco de dados

--- Blog/Data/BlogDataContext.cs
+++ Blog/Data/BlogDataContext.cs
@@ -1,16 +1,20 @@
 ﻿using Blog.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Blog.Data
 {
     public class BlogDataContext : DbContext
     {
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        //public DbSet<PostTag> PostTags { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+        //public DbSet<UserRole> UserRoles{ get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("Data Source=LAPTOP-563RGJKO\\SQLEXPRESS;Initial Catalog=blog-modulo-6; Integrated Security=True");
+            //=> options.UseSqlServer("Server = LAPTOP-563RGJKO\\SQLEXPRESS; Database=blog-modulo-6;User Id = sa; Password=@Sql2019;");
     }
 }
--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Blog.Data;
+using System.Linq;
 
 namespace Blog
 {
@@ -6,7 +7,22 @@ namespace Blog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using(var context = new BlogDataContext())
+            {
+                /*var tag = new Tag { Name = "Asp.Net", Slug = "aspnet" };
+                context.Tags.Add(tag);
+                context.SaveChanges();*/
+
+                var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
+                tag.Name = "ASP.Net";
+                tag.Slug = "aspnet";
+
+                context.Update(tag);
+                context.SaveChanges();
+
+
+
+            }
         }
     }
 }
